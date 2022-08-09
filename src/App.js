edc30ea5a47c0f221d3854e07a288accb1040f93
PaulPintang <pauljpintang@gmail.com>
2022-08-09 10:43:07 +0800

    feat: google sign in

--- src/App.js
+++ src/App.js
@@ -1,14 +1,37 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import jwtDecode from "jwt-decode";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
+
 import "./App.css";
 
 function App() {
+  const [user, setUser] = useState();
+
+  const createOrGetUser = (res) => {
+    const decoded = jwtDecode(res.credential);
+    console.log(decoded);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <button>Sign in with google</button>
-      </header>
-    </div>
+    <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_API_TOKEN}>
+      <div className="App">
+        <header className="App-header">
+          <button onClick={googleLogout()}>LOGOUT</button>
+          {user ? (
+            <div>Logged in</div>
+          ) : (
+            <GoogleLogin
+              onSuccess={(res) => createOrGetUser(res)}
+              onError={() => {
+                console.log("Login Failed");
+              }}
+            />
+          )}
+        </header>
+      </div>
+    </GoogleOAuthProvider>
   );
 }
+googleLogout();
 
 export default App;
